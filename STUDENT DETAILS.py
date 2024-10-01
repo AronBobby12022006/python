04c62ea1a57f4d9779d2e45fab00cde7555da591
AronBobby12022006 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-10-01 14:52:43 +0530

    Add files via upload

---
+++ STUDENT DETAILS.py
@@ -0,0 +1,7 @@
+name=input("enter the student name")
+roll_no=int(input("enter your reg no"))
+roll_no=roll_no+1
+cgpa=float(input("enter your cgpa"))
+print("the name of the student is:",name)
+print("roll_no of the student:",roll_no)
+print("CGPA:",cgpa,"%")
