6cbc0c3ea8dd6184945dbeca6a72ef4381e94e04
AronBobby12022006 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-10-01 15:30:09 +0530

    Add files via upload
    programme header added

--- STUDENT DETAILS.py
+++ STUDENT DETAILS.py
@@ -6,6 +6,7 @@ name=input("enter the student name")
 roll_no=int(input("enter your reg no"))
 roll_no=roll_no+1
 cgpa=float(input("enter your cgpa"))
+result=cgpa*10
 print("the name of the student is:",name)
 print("roll_no of the student:",roll_no)
-print("CGPA:",cgpa,"%")
+print("CGPA:",result,"%")
